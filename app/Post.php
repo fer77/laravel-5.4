77ace220ad1076989d516dcd628f0b963368da50
Fernando Fernandez <fernando.fernandez@chghealthcare.com>
2017-07-24 15:07:56 -0600

    mapping eloquent events to dedicated classes

--- app/Post.php
+++ app/Post.php
@@ -5,14 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  public function archive()
-  {
-    $this->update(['active' => false]);
-  }
-
-  public function activate()
-  {
-    $this->update(['active' => true]);
-  }
+  protected $events = [
+    'created'=>PostWasPublished::class
+  ];
 }
  ?>
